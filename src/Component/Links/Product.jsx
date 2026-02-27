12b1680cfd5a36c24554af7552764f2eec1b6b90
Shubham <shubnegi1004@gmail.com>
2026-02-27 10:39:10 +0530

    Made Product Page

--- src/Component/Links/Product.jsx
+++ src/Component/Links/Product.jsx
@@ -2,7 +2,161 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <div className='flex flex-wrap gap-2 space-y-5 mt-3.5 justify-center items-start '>
+      
+      <div className='w-full flex  justify-center items-start flex-col align-middle'> 
+      <div className='font-bold text-[25px] ml-2.5 pl-2.5'>
+        <h1  >Health Product's</h1>
+      </div>
+
+      <div className='flex flex-wrap gap-8 m-2.5 p-2.5 justify-center items-center '>        
+      <a href="">
+        <img className=' object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61OWZVmAiDL._SX679_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className=' object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71NTdqTLhlL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71gedmM6o3L._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-cover max-h-[220px] max-w-[200px]' src="https://m.media-amazon.com/images/I/31e2F40yu7L._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61Qi9g2NxWL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className=' object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61b+TLk8WwL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className=' object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71z3wzINlHL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+      </div>
+
+      </div>
+
+      {/* Snacks */}
+
+      <div className='w-full flex  justify-center items-start   align-middle flex-col'>
+        <div className='font-bold text-[25px] ml-2.5 pl-2.5' >
+          <h1>Healthy snacks Alternative </h1>
+        </div>
+
+      <div className='flex flex-wrap gap-8 m-2.5 p-2.5 justify-center items-center'>
+      <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61aS0Qtm5WL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-cover max-h-[220px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71tydQwOa0L._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-cover max-h-[220px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71Sq8ly3TuL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71L2i0mVKQL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61aAcM3ttYL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61QhySXRJ4L._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+       <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71JJZWedEJL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+      </div>
+
+    </div>
+
+    {/* Juice */}
+
+    <div className='w-full flex  justify-center items-start   align-middle flex-col'>
+        <div className='font-bold text-[25px] ml-2.5 pl-2.5' >
+          <h1>Healthy Juice's Alternative </h1>
+        </div>
+
+      <div className='flex flex-wrap gap-8 m-2.5 p-2.5 justify-center items-center'>
+      <a href="">
+        <img className='object-contain h-[270px] w-[200px] max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71-Z5GxReLL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-contain max-h-[220px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71C6ZjO7EJL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61cyWetM2QL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/610BC1qynIL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61CGxX79xRL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/41U0PIm4qFS._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+       <a href="">
+        <img className='object-contain max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61d3H3w9TXL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+      </div>
+
+    </div>
+
+    <div className='w-full flex  justify-center items-start   align-middle flex-col'>
+        <div className='font-bold text-[25px] ml-2.5 pl-2.5' >
+          <h1>Healthy snacks Alternativ </h1>
+        </div>
+
+      <div className='flex flex-wrap gap-8 m-2.5 p-2.5 justify-center items-center'>
+      <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61aS0Qtm5WL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-cover max-h-[220px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71tydQwOa0L._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='object-cover max-h-[220px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71Sq8ly3TuL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71L2i0mVKQL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61aAcM3ttYL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+      <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/61QhySXRJ4L._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+
+       <a href="">
+        <img className='max-h-[270px] max-w-[200px]' src="https://m.media-amazon.com/images/I/71JJZWedEJL._AC_UL480_FMwebp_QL65_.jpg" alt="CooperBottle" />
+      </a>
+      </div>
+
+    </div>
+
+    </div>
   )
 }
 

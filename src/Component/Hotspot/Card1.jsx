301342bdb8ad8d88cbd8ffb590614b1c52e362bf
Shubham <shubnegi1004@gmail.com>
2026-03-20 22:46:45 +0530

    added obisity page navigation and content in the navigated page

--- src/Component/Hotspot/Card1.jsx
+++ src/Component/Hotspot/Card1.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Card1 = ({ image, title, description }) => {
+const Card1 = ({ image, title, description, onClick }) => {
   return (
     <div
       className="
@@ -18,6 +18,7 @@ const Card1 = ({ image, title, description }) => {
         cursor-pointer
         mx-auto
       "
+      onClick={onClick}
     >
       {/* Image */}
       <img
